2c6b7b138c4cf96bde3dc2b568b9b1330056f2df
suin0615 <k72290560@gmail.com>
2020-09-22 15:31:51 +0900

    bot upload

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on("guildMemberRemove", (member) => {
 client.on('message', (message) => {
   if(message.author.bot) return;
 
-  if(message.content == '%ping') {
+  if(message.content == 'pping') {
     return message.reply('pong');
   }
 
